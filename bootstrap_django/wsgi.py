3db174f0a0cd8ef63aa9f198d2b876f610586b9b
Polin Liao <prob1995@gmail.com>
2015-05-05 17:12:41 +0000

    SM on heroku

--- bootstrap_django/wsgi.py
+++ bootstrap_django/wsgi.py
@@ -12,3 +12,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootstrap_django.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+#FOR HEROKU DEPLOY
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
